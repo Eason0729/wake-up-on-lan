95d9bc25a6e50ed375a692171017ed157ae9cd7a
eason <easonqq0000@gmail.com>
2022-07-15 02:19:35 +0000

    test ci

--- src/index.rs
+++ src/index.rs
@@ -1,9 +1,8 @@
-use actix_files ;
-use actix_web::{ Error, HttpRequest};
+use actix_files;
+use actix_web::{Error, HttpRequest};
 
 pub async fn index_handler(_req: HttpRequest) -> Result<actix_files::NamedFile, Error> {
     let path = std::path::PathBuf::from("./public/index.html");
     let file = actix_files::NamedFile::open(path)?;
-    Ok(file
-        .use_last_modified(true))
+    Ok(file.use_last_modified(true))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod config;
-mod password;
 mod index;
+mod password;
 mod wake;
 
-use crate::{password::password_handler};
+use crate::index::index_handler;
+use crate::password::password_handler;
 use actix_files;
 use actix_web::{web, App, HttpServer};
 use config::Config;
 use std::fs;
-use crate::index::index_handler;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -24,10 +24,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(config.clone()))
             .route("/wake", web::post().to(password_handler))
             .route("/", web::get().to(index_handler))
-            .service(
-                actix_files::Files::new("/", "./public")
-                    .use_last_modified(false),
-            )
+            .service(actix_files::Files::new("/", "./public").use_last_modified(false))
     })
     .bind(("0.0.0.0", port))?
     .run()
