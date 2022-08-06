7d6d461739b29f328ddfcc8b1d582189debcdd17
eason <easonqq0000@gmail.com>
2022-08-06 04:34:15 +0000

    trim index.html

--- src/index.rs
+++ src/index.rs
@@ -1,7 +1,15 @@
 use actix_web::{http::header::ContentType, HttpRequest, HttpResponse, Responder};
 
 pub async fn index_handler(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .body(include_str!("../index.html").trim())
+    HttpResponse::Ok().content_type(ContentType::html()).body(
+        include_str!("../index.html")
+            .lines()
+            .collect::<Vec<_>>()
+            .join("")
+            .trim()
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .collect::<Vec<_>>()
+            .join(" "),
+    )
 }
