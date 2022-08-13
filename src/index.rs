6db26b2e6e304aab0cab8aff9d8b5a8e5a39b893
eason <easonqq0000@gmail.com>
2022-08-13 10:41:33 +0000

    rewrite UI and add build.rs(for html minify)

--- src/index.rs
+++ src/index.rs
@@ -1,15 +1,7 @@
 use actix_web::{http::header::ContentType, HttpRequest, HttpResponse, Responder};
 
 pub async fn index_handler(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().content_type(ContentType::html()).body(
-        include_str!("../index.html")
-            .lines()
-            .collect::<Vec<_>>()
-            .join("")
-            .trim()
-            .split(' ')
-            .filter(|s| !s.is_empty())
-            .collect::<Vec<_>>()
-            .join(" "),
-    )
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(include_str!("../tmp/static/index.html"))
 }
